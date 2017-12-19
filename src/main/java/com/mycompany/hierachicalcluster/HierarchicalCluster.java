f68c1e3940bf661f6e53be89aef38a271039520d
renansantos <renansantos@renansantos-Lenovo-G480>
2017-12-19 21:51:52 -0200

    - olhar esse codigo, usar o WEKA

--- src/main/java/com/mycompany/hierachicalcluster/HierarchicalCluster.java
+++ src/main/java/com/mycompany/hierachicalcluster/HierarchicalCluster.java
@@ -1,15 +1,52 @@
 package com.mycompany.hierachicalcluster;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author renansantos
  */
 public class HierarchicalCluster {
+
+    private Object[][] integerData;
+    private String fileName;
+    private int numberOfClusters;
+    private List<Cluster> clusters;
     
+    private class Cluster{
+        private List<Integer> points;
+        
+        public List<Integer> getPoints(){
+            return this.points;
+        }
+    }
+
+    public HierarchicalCluster(int numberOfClusters, Integer[][] integerData) {
+        this.integerData = integerData;
+        this.numberOfClusters = numberOfClusters;
+        this.clusters = new ArrayList<>();
+    }
+    
+     public HierarchicalCluster(int numberOfClusters, Double[][] integerData) {
+        this.integerData = integerData;
+        this.numberOfClusters = numberOfClusters;
+        this.clusters = new ArrayList<>();
+    }
+     
+     public HierarchicalCluster(String fileName, int numberOfClusters) {
+        //this.integerData = integerData;
+        this.fileName = fileName;
+        this.numberOfClusters = numberOfClusters;
+        this.clusters = new ArrayList<>();
+    }
+    
+    
+    
+
 }
---
+++ src/main/java/com/mycompany/hierachicalcluster/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hierachicalcluster;
+
+/**
+ *
+ * @author renansantos
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String path = "/home/renansantos/NetBeansProjects/VRPDRTGoogleMapsAPI/AlgorithmsResults/9FO/NSGA-II/r050n12tw10k4_2017_12_14";
+        String file = "NSGAII-Population_0.csv";
+        Integer[][] data = new Integer[3][3];
+        //new HierachicalCluster(3,data);
+    }
+}
