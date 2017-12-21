30fa80fd842a3b34dd67d770439cc58b384ff448
renansantos <renansantos@renansantos-Lenovo-G480>
2017-12-20 22:34:30 -0200

    - início da implementação do HC

--- src/main/java/com/mycompany/hierachicalcluster/HierarchicalCluster.java
+++ src/main/java/com/mycompany/hierachicalcluster/HierarchicalCluster.java
@@ -1,5 +1,9 @@
 package com.mycompany.hierachicalcluster;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,39 +18,60 @@ import java.util.List;
  */
 public class HierarchicalCluster {
 
-    private Object[][] integerData;
+    private List<List<Double>> listData;
+    private Double[][] data;
     private String fileName;
     private int numberOfClusters;
     private List<Cluster> clusters;
-    
-    private class Cluster{
-        private List<Integer> points;
-        
-        public List<Integer> getPoints(){
+    private Double[][] similarity;
+
+    private class Cluster {
+
+        private List<Double> points;
+
+        public List<Double> getPoints() {
             return this.points;
         }
     }
 
-    public HierarchicalCluster(int numberOfClusters, Integer[][] integerData) {
-        this.integerData = integerData;
+    public HierarchicalCluster(int numberOfClusters, Double[][] data) {
+        this.data = data;
         this.numberOfClusters = numberOfClusters;
         this.clusters = new ArrayList<>();
     }
-    
-     public HierarchicalCluster(int numberOfClusters, Double[][] integerData) {
-        this.integerData = integerData;
-        this.numberOfClusters = numberOfClusters;
-        this.clusters = new ArrayList<>();
-    }
-     
-     public HierarchicalCluster(String fileName, int numberOfClusters) {
-        //this.integerData = integerData;
+
+    public HierarchicalCluster(String fileName, int numberOfClusters) throws IOException {
         this.fileName = fileName;
         this.numberOfClusters = numberOfClusters;
         this.clusters = new ArrayList<>();
+        this.listData = readData();
+        
+        calculateSilimarity();
     }
-    
-    
-    
 
+    private List<List<Double>> readData() throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        List<List<Double>> listData = new ArrayList<>();
+        while (br.ready()) {
+            String linha = br.readLine();
+            String[] teste = linha.split(",");
+            List<Double> line = new ArrayList<>();
+
+            for (String string : teste) {
+                line.add(Double.parseDouble(string));
+            }
+            System.out.println(line);
+            listData.add(line);
+        }
+        
+        System.out.println("Number of rows = " + listData.size());
+        System.out.println("Number of columns = " + listData.get(0).size());
+        br.close();
+
+        return listData;
+    }
+
+    private void calculateSilimarity(){
+        
+    }
 }
--- src/main/java/com/mycompany/hierachicalcluster/Main.java
+++ src/main/java/com/mycompany/hierachicalcluster/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.mycompany.hierachicalcluster;
+import java.io.IOException;
 
 /**
  *
@@ -11,10 +12,27 @@ package com.mycompany.hierachicalcluster;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String path = "/home/renansantos/NetBeansProjects/VRPDRTGoogleMapsAPI/AlgorithmsResults/9FO/NSGA-II/r050n12tw10k4_2017_12_14";
         String file = "NSGAII-Population_0.csv";
-        Integer[][] data = new Integer[3][3];
-        //new HierachicalCluster(3,data);
+        new HierarchicalCluster(path+"/"+file,3);
+
+//        BufferedReader br = new BufferedReader(new FileReader(path+"/"+file));
+//        List<List<Double>> listData = new ArrayList<>();
+//        while (br.ready()) {
+//            String linha = br.readLine();
+//            String [] teste = linha.split(",");//Double.parseDouble(string)
+//            List<Double> line = new ArrayList<>();
+//            
+//            for(String string: teste){
+//                line.add(Double.parseDouble(string));
+//            }
+//            System.out.println(line);
+//            listData.add(line);
+//        }
+//        System.out.println("Number of rows = " + listData.size());
+//        System.out.println("Number of columns = " + listData.get(0).size());
+//        
+//        br.close();
     }
 }
